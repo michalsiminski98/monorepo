54ced02f7e741fe9149afa6795b96720b020247f
Michal Siminski <michal.siminski@a4bee.com>
2023-08-08 15:28:35 +0200

    i18

--- libs/main-content/src/lib/main-content.spec.tsx
+++ libs/main-content/src/lib/main-content.spec.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 
-import MainContent from './main-content';
+import { MainContent } from './main-content';
 
 describe('MainContent', () => {
   it('should render successfully', () => {
